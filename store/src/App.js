538957a27359691451e297a001cb9d27bc603d40
greyewi1 <greyewi@gmail.com>
2021-05-16 18:13:13 +0500

    fix router

--- store/src/App.js
+++ store/src/App.js
@@ -1,36 +1,36 @@
-import './App.css';
+import './App.css'
 import {useState} from 'react'
 import ProductList from './components/ProductsList'
 import CreateProducts from "./components/CreateProducts"
 import BucketList from "./components/BusketList"
-import {BrowserRouter as Switch, Route, Link} from "react-router-dom"
+import {Switch, Link, Route} from "react-router-dom"
 
 function App() {
   const [productList, setProductList] = useState([])
   const [bucketList, setBucketList] = useState([])
-  
+
   return (
     <div className="App">
       <Link to={"/create"}> Create</Link>
-      <Link to={"/products"} >Prod</Link>
+      <Link to={"/products"}>Prod</Link>
       <Link to={"/buskets"}> Busk</Link>
       <Switch>
-      <Route exact path="/create">
-         <CreateProducts setProductList={setProductList}/>
-      </Route>
+        <Route path="/create">
+          <CreateProducts setProductList={setProductList}/>
+        </Route>
 
-      <Route path="/products">
+        <Route path="/products">
           <ProductList productList={productList} setBucketList={setBucketList}/>
-      </Route>
-      <Route path="/buskets">
+        </Route>
+        <Route path="/buskets">
           <BucketList bucketList={bucketList}/>
-      </Route>
-        
-        </Switch>
-     
-      
+        </Route>
+
+      </Switch>
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
