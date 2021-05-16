d0ddf01b2f76b59e8cfb9f09c005968087298c9a
unknown <bulgackoff.denis@yandex.ru>
2021-05-16 14:43:55 +0300

    Router

--- store/src/App.js
+++ store/src/App.js
@@ -3,17 +3,32 @@ import {useState} from 'react'
 import ProductList from './components/ProductsList'
 import CreateProducts from "./components/CreateProducts"
 import BucketList from "./components/BusketList"
+import {BrowserRouter as Switch, Route, Link} from "react-router-dom"
 
 function App() {
   const [productList, setProductList] = useState([])
   const [bucketList, setBucketList] = useState([])
-  const [tempList, setTempList] = useState([])
-
+  
   return (
     <div className="App">
-      <CreateProducts setProductList={setProductList}/>
-      <ProductList productList={productList} setBucketList={setBucketList}/>
-      <BucketList bucketList={bucketList}/>
+      <Link to={"/create"}> Create</Link>
+      <Link to={"/products"} >Prod</Link>
+      <Link to={"/buskets"}> Busk</Link>
+      <Switch>
+      <Route exact path="/create">
+         <CreateProducts setProductList={setProductList}/>
+      </Route>
+
+      <Route path="/products">
+          <ProductList productList={productList} setBucketList={setBucketList}/>
+      </Route>
+      <Route path="/buskets">
+          <BucketList bucketList={bucketList}/>
+      </Route>
+        
+        </Switch>
+     
+      
     </div>
   );
 }
